a78696acfa30c7cb2cf214764a2619df00576de4
timo <timo.hermans@outlook.com>
2016-09-10 12:04:09 +0200

    Add the delete function for deck and only return non deleted decks

--- routes/deck.js
+++ routes/deck.js
@@ -2,51 +2,80 @@ const models = require('../models');
 const Boom = require('boom');
 
 module.exports = function (hapiServer) {
-  hapiServer.route({
-    method: 'GET',
-    path: '/deck',
-    handler: (request, reply) => {
-      models.Deck.findAll().then(function (decks) {
-        reply(decks);
-      });
-    }
-  });
-
-  hapiServer.route({
-    method: 'GET',
-    path: '/deck/{id}',
-    handler: (request, reply) => {
-      var id = encodeURIComponent(request.params.id);
-
-      models.Deck.findById(id).then(function (deck) {
-        if (!deck) {
-          reply({ error: 'no deck found' });
+    /**
+     * Retrieve all decks that are not deleted
+     */
+    hapiServer.route({
+        method: 'GET',
+        path: '/deck',
+        handler: (request, reply) => {
+            models.Deck.findAll({
+                where: {
+                    isDeleted: false
+                }
+            }).then(function (decks) {
+                reply(decks);
+            });
         }
-        else {
-          reply(deck);
+    });
+
+    /**
+     * Get a deck by ID
+     */
+    hapiServer.route({
+        method: 'GET',
+        path: '/deck/{id}',
+        handler: (request, reply) => {
+            var id = encodeURIComponent(request.params.id);
+
+            models.Deck.findById(id).then(function (deck) {
+                if (!deck) {
+                    reply({error: 'no deck found'});
+                }
+                else {
+                    reply(deck);
+                }
+            });
         }
-      });
-    }
-  });
+    });
 
+    /**
+     * Add a deck to the database
+     */
+    hapiServer.route({
+        method: 'POST',
+        path: '/deck',
+        handler: (request, reply) => {
+            var payload = request.payload;
 
-  hapiServer.route({
-    method: 'POST',
-    path: '/deck',
-    handler: (request, reply) => {
-      var payload = request.payload;
+            if (!payload || !payload.name) {
+                const error = Boom.badRequest('invalid deck object');
+                reply(error);
+            }
+            else {
+                models.Deck.create(payload).then((deck) => {
+                    reply(deck);
+                }).catch(function (error) {
+                    reply(Boom.badImplementation(error));
+                });
+            }
+        }
+    });
 
-      if (!payload || !payload.name) {
-        const error = Boom.badRequest('invalid deck object');
-        reply(error);
-      }
-      else {
-        models.Deck.create(payload).then((deck) => {
-          reply(deck);
-        }).catch(function(error) {
-          reply(Boom.badImplementation(error));
-        });
-      }
-    }
-  })
+    /**
+     * Mark a deck as deleted from the database
+     */
+    hapiServer.route({
+        method: 'DELETE',
+        path: '/deck/{id}',
+        handler: (request, reply) => {
+            var id = encodeURIComponent(request.params.id);
+
+            models.Deck.findById(id).then(deck => {
+                deck.isDeleted = true;
+                deck.save()
+                    .then(() => reply('ok'), error => reply(Boom.badImplementation(error))); 
+            }, error => reply(Boom.badImplementation(error))); 
+        }
+    })
 };
