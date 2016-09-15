14b20a911786931d3f4e644f256e1092519efd8e
Timo Hermans <tmhermans@gmail.com>
2016-09-15 13:27:11 +0200

    Add kanji search to the search route and make sure that the application waits for every async task to complete before moving on

--- routes/search.js
+++ routes/search.js
@@ -1,5 +1,56 @@
 const models = require('../models');
 const Boom = require('boom');
+const Promise = require('bluebird');
+const _ = require('lodash');
+
+//private function
+/**
+ * perform a radical search on a word that is split up into character
+ * So it basically does a LIKE ANY. Not supported in SQLite 3 :(
+ * @param keyword string
+ * @param result [any]
+ * @returns {Promise.<TResult>}
+ * @private
+ */
+var _doRadicalSearch = function (keyword, result) {
+  //split the word up in smaller pieces
+  var wordFilter = _.split(keyword, '');
+
+  return models.KanjiRadical.findAll({
+    where: {
+      kanji: {
+        $like: { $any: wordFilter }
+      }
+    },
+    order: [
+      ['kanji', 'ASC']
+    ]
+  }).then(kanjiRadicals => {
+    result.radicals = kanjiRadicals;
+  });
+};
+
+/**
+ * Do a search on each character in the word on the kanji database
+ * @param keyword string
+ * @param result [any]
+ * @returns {Promise.<null>}
+ * @private
+ */
+var _doKanjiSearch = function (keyword, result) {
+  var wordFilter = _.split(keyword, '');
+
+  return models.Kanji.findAll({
+    where: {
+      character: {
+        $in: wordFilter
+      }
+    }
+  })
+    .then(function (kanjis) {
+      result.kanji = kanjis;
+    });
+}
 
 module.exports = function (hapiServer) {
   /**
@@ -15,24 +66,14 @@ module.exports = function (hapiServer) {
         const error = Boom.badRequest('No keyword sent');
         return reply(error);
       }
+      var result = {};
 
-      //split the word up in smaller pieces
-      var wordFilter = keyword.split('');
-
+      var actions = [];
       //get kanji radicals first
-      models.KanjiRadical.findAll({
-        where: {
-          kanji: {
-            $like: { $any: wordFilter }
-          }
-        },
-        order: [
-          ['kanji', 'ASC']
-        ]
-      }).then(kanjiRadicals => {
-        console.log(kanjiRadicals);
-        reply({ radicals: kanjiRadicals })
-      });
+      actions.push(_doRadicalSearch(keyword, result));
+      actions.push(_doKanjiSearch(keyword, result));
+
+      Promise.all(actions).then(() => reply(result));
     }
   });
 }
